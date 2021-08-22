b587dc4c873ae5d1cf3f95bf4a1ddd9d19263d82
Raymond <raymond@philippinedev.com>
2021-08-22 17:42:53 +0800

    initial, devise, and seller view

---
+++ app/controllers/flowers_controller.rb
@@ -0,0 +1,36 @@
+class FlowersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_flower, only: [:destroy]
+
+  def index
+    @flowers = Flower.all
+  end
+
+  def new
+    @flower = Flower.new
+  end
+
+  def create
+    @flower = Flower.new(flower_params)
+    if @flower.save
+      redirect_to flowers_path, notice: "#{@flower.name} successfully added"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @flower.destroy
+      redirect_to flowers_path, alert: "#{@flower.name} removed from the shop"
+  end
+
+  private
+
+  def flower_params
+    params.require(:flower).permit(:name, :qty, :price)
+  end
+
+  def set_flower
+    @flower = Flower.find(params[:id])
+  end
+end
